f5575e45d7188dd581909f15a26c9b4aca91cac6
Rob Mor <i20B3RT@users.noreply.github.com>
2018-01-22 23:19:42 -0500

    Added Test for Customer and product.

--- src/test/java/com/uscold/mdmrepointaqa/test/inventory/InventorySearchTest.java
+++ src/test/java/com/uscold/mdmrepointaqa/test/inventory/InventorySearchTest.java
@@ -11,10 +11,12 @@ import org.testng.annotations.Test;
 
 public class InventorySearchTest extends AbstractTestClass {
 
-    final static String WHSE_NUM = "800";
-    final static String CUSTOMER_NAME = "1017900 - CUSTOMER FOR AUTOMATION";
+    final static String WHSE = "800 - BETHLEHEM";
+    final static String CU = "100100";
+    int cu_num =Integer.parseInt(CU);
+    final static String C_NAME ="ACE BAKERY LTD";
     final static String USCS_Lot = "425309";
-    final static String PRODUCT_CODE = "777";
+    final static String P_CODE = "41208";
     final static String Location = "01B 0014";
     final static String License = "41019001";
 //License 41019001 is in WIP status
@@ -23,66 +25,61 @@ public class InventorySearchTest extends AbstractTestClass {
     @Test
     public void searchWarehouses() throws InterruptedException {
         PageHelper.chooseModule(driver, "Warehouse Management");
-        driver.findElement(By.id("txt_searchNumber")).sendKeys(WHSE_NUM);
+        driver.findElement(By.id("txt_searchNumber")).sendKeys(WHSE);
         click(driver.findElement(By.id("searchOne")));
-//        WebElement spiner = driver.findElement(By.id("gbox_list"));
+        WebElement spiner = driver.findElement(By.id("load_list"));
+        wait.until(ExpectedConditions.invisibilityOf(spiner));
+//        driver.findElement(By.id("OEDCREATEPOPUP-1_gCol_pCode")).sendKeys(PRODUCT_CODE.substring(0, 3));
+
+        String cst = driver.findElement(By.xpath("//td[@title='" + WHSE.substring(0,3) + "'][1]")).getText();
+        Assert.assertEquals(cst, WHSE.substring(0,3));
+    }
+
+    @Test
+    public void searchCustomer() throws InterruptedException {
+        PageHelper.chooseModule(driver, "Customer Management");
+        PageHelper.chooseWarehouse(driver,WHSE);
+        PageHelper.chooseCustomer(driver,cu_num);
+        click(driver.findElement(By.id("searchOne")));
+        WebElement spiner = driver.findElement(By.id("load_list"));
+        wait.until(ExpectedConditions.invisibilityOf(spiner));
+
+        String cst = driver.findElement(By.xpath("//td[@title='" + C_NAME + "'][1]")).getText();
+        Assert.assertEquals(cst, C_NAME);
+
+//        click(driver.findElement(By.xpath("//span[@class='field_val selectCustomer']")));
+//        driver.findElement(By.xpath("//span[@class='field_val selectCustomer']//input[@type='text']")).sendKeys(CUSTOMER_NAME);
+//        click(driver.findElement(By.id("basicSrch")));
+//        WebElement spiner = driver.findElement(By.id("load_productGrid"));
 //        wait.until(ExpectedConditions.invisibilityOf(spiner));
-        String cst = driver.findElement(By.xpath("//td/a")).getText();
-        Assert.assertEquals(cst, WHSE_NUM);
-    }
-
-    @Test
-    public void searchByCustomer() throws InterruptedException {
-        PageHelper.chooseModule(driver, "Inventory Maintenance");
-        PageHelper.chooseWarehouse(driver,800);
-        PageHelper.chooseCustomer(driver,100500);
-        click(driver.findElement(By.xpath("//span[@class='field_val selectCustomer']")));
-        driver.findElement(By.xpath("//span[@class='field_val selectCustomer']//input[@type='text']")).sendKeys(CUSTOMER_NAME);
-        click(driver.findElement(By.id("basicSrch")));
-        WebElement spiner = driver.findElement(By.id("load_productGrid"));
-        wait.until(ExpectedConditions.invisibilityOf(spiner));
-        String cst = driver.findElement(By.xpath("//td[@title='1017900 - CUSTOMER FOR AUTOMATION']")).getText();
-        Assert.assertEquals(cst, CUSTOMER_NAME);
+//        String cst = driver.findElement(By.xpath("//td[@title='1017900 - CUSTOMER FOR AUTOMATION']")).getText();
+//        Assert.assertEquals(cst, CUSTOMER_NAME);
     }
 
 
 
     @Test
-    public void searchByProduct() throws InterruptedException {
-        PageHelper.chooseModule(driver, "Inventory Maintenance");
-        click(PageHelper.chooseValueFromStandardDropDownBySubstring(driver, "pem_cust_chosen", "select"));
-
-        driver.findElement(By.id("txt_SubProduct")).sendKeys(PRODUCT_CODE);
-        click(driver.findElement(By.id("basicSrch")));
-        WebElement spiner = driver.findElement(By.id("load_productGrid"));
+    public void searchProduct() throws InterruptedException {
+        PageHelper.chooseModule(driver, "Product Management");
+        PageHelper.chooseWarehouse(driver,WHSE);
+//        PageHelper.chooseCustomer(driver,cu_num);
+        driver.findElement(By.id("codeOrDesc")).sendKeys(P_CODE);
+        click(driver.findElement(By.id("searchOne")));
+        WebElement spiner = driver.findElement(By.id("load_list"));
         wait.until(ExpectedConditions.invisibilityOf(spiner));
-        String cst = driver.findElement(By.xpath("//td[@title='777']")).getAttribute("innerText");
-        Assert.assertEquals(cst, PRODUCT_CODE);
+        String cst = driver.findElement(By.xpath("//td[@title='" + P_CODE + "'][1]")).getText();
+        Assert.assertEquals(cst, P_CODE);
+
+
+//        PageHelper.chooseModule(driver, "Inventory Maintenance");
+//        click(PageHelper.chooseValueFromStandardDropDownBySubstring(driver, "pem_cust_chosen", "select"));
+//
+//        driver.findElement(By.id("txt_SubProduct")).sendKeys(PRODUCT_CODE);
+//        click(driver.findElement(By.id("basicSrch")));
+//        WebElement spiner = driver.findElement(By.id("load_productGrid"));
+//        wait.until(ExpectedConditions.invisibilityOf(spiner));
+//        String cst = driver.findElement(By.xpath("//td[@title='777']")).getAttribute("innerText");
+//        Assert.assertEquals(cst, PRODUCT_CODE);
     }
 
-
-    @Test
-    public void searchByLocation() throws InterruptedException {
-        PageHelper.chooseModule(driver,"Inventory Maintenance");
-        click(PageHelper.chooseValueFromStandardDropDownBySubstring(driver, "pem_cust_chosen", "select"));
-        driver.findElement(By.id("location")).sendKeys(Location);
-        click(driver.findElement(By.id("basicSrch")));
-        WebElement spiner = driver.findElement(By.id("load_licenseGrid"));
-        wait.until(ExpectedConditions.invisibilityOf(spiner));
-        String cst = driver.findElement(By.xpath("//td[normalize-space() = '01B 0014']")).getText();
-        Assert.assertEquals(cst, Location);
-    }
-
-
-    @Test
-    public void searchByLicense() throws InterruptedException {
-        PageHelper.chooseModule(driver,"Inventory Maintenance");
-        click(PageHelper.chooseValueFromStandardDropDownBySubstring(driver, "pem_cust_chosen", "select"));
-        driver.findElement(By.id("License")).sendKeys(License);
-        click(driver.findElement(By.id("basicSrch")));
-        WebElement spiner = driver.findElement(By.id("load_licenseGrid"));
-        wait.until(ExpectedConditions.invisibilityOf(spiner));
-        String cst = driver.findElement(By.xpath("//td[@title='41019001']")).getText();
-        Assert.assertEquals(cst, License);
-    }
 }
