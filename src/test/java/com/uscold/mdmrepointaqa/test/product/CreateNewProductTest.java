6a6bf9d1580abd7ef7e4267134fa0cb78dec3eab
Robert Morales <i20B3RT@live.com>
2018-01-23 20:36:10 -0500

    Added product code package and product class

---
+++ src/test/java/com/uscold/mdmrepointaqa/test/product/CreateNewProductTest.java
@@ -0,0 +1,9 @@
+package com.uscold.mdmrepointaqa.test.product;
+
+/**
+ * ********************************************
+ * * Created by ${"Robert Morales"} on 1/23/2018.
+ * ********************************************
+ **/
+public class CreateNewProductTest {
+}
